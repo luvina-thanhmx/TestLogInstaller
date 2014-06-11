2ffef39b8339e66167080d053c177539d5f8023e
ThanhMX <mrtit@thanhmx-luvina.(none)>
2014-06-11 14:36:33 +0700

    Update source test

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-TestLogInstaller
-================
+Source test for create wiperdog installation log file (#411).
+---------------------------------------------
+A. How to test?  
+- Step1: run ./run_test.sh with format  
+	*Run each case: ./run_test.sh -p /home/mrtit/Wiperdog/1205Wiperdog/ -c Case1  
+	*Run all case: ./run_test.sh -p /home/mrtit/Wiperdog/1205Wiperdog/ -c all  
+- Step2: Check result  
+
+B. Case test  
+
+ 1. Test write log when install with value of parameters without default (Case1)  
+  - Input: Value of parameters without default (ex: jetty port is 123456, mongo host is 10.0.0.2).  
+  - Expected: in the log file will show value of parameters corresponding to set.  
+  
+ 2. Test write log when install have error (Case2)  
+  - Input: Value of jetty port to set is not number.  
+  - Expected: in the log file will show the message about error.  
+
+ 3. Test write log when install with value of parameters is default (Case3)  
+  - Input: Value of parameters is default.  
+  - Expected: in the log file will show value of parameters is default.  
+
--- run_test.groovy
+++ run_test.groovy
@@ -19,69 +19,100 @@ public class TestJobServlet{
 			}
 			
 			// check value of -c parameter
-			if(caseTest == null || caseTest == "" || !caseTest.contains("Case")) {
+			if(caseTest == null || caseTest == "") {
 				println "Incorrect parameters. Please check again value of -c."
 				return
 			}
+			// path to jar file
 			def jarFile = pathToWiperdog + "target/wiperdog-0.2.5-SNAPSHOT-unix.jar"
-			if(!(new File(jarFile)).exists()) {
-				// build wiperdog install
-				listCmd = new LinkedList<String>();
-				listCmd.add("mvn")
-				listCmd.add("clean")
-				listCmd.add("install")
-				println "building wiperdog..."
-				output = runProcClosure(listCmd, new File(pathToWiperdog), true)
-				if (output.contains("BUILD SUCCESS")){
-					println "Build wiperdog successfully!"
-				} else {
-					println "Build wiperdog failure!"
-					return
-				}
-			}
-		
+
+			// build wiperdog install
 			listCmd = new LinkedList<String>();
-			listCmd.add("java")
-			listCmd.add("-jar")
-			listCmd.add(jarFile)
-			listCmd.add("-d")
-			listCmd.add(dirFolderTest + "/WiperdogTest")
-			listCmd.add("-j")
-			if (caseTest != "Case2") {//set jetty port is number
-				listCmd.add("123456")	
-			} else {//set jetty port is not number
-				listCmd.add("error")
-			}
-			if (caseTest != "Case3") {
-				listCmd.add("-m")
-				listCmd.add("10.0.0.2")
-				listCmd.add("-p")
-				listCmd.add("28017")
-				listCmd.add("-n")
-				listCmd.add("wiperdog")
-				listCmd.add("-u")
-				listCmd.add("sa")
-				listCmd.add("-pw")
-				listCmd.add("insight")
-				listCmd.add("-mp")
-				listCmd.add("test@gmail.com")
-				listCmd.add("-s")
-				listCmd.add("no")
+			listCmd.add("mvn")
+			listCmd.add("clean")
+			listCmd.add("install")
+			println "building wiperdog..."
+			output = runProcClosure(listCmd, new File(pathToWiperdog), true)
+			if (output.contains("BUILD SUCCESS")){
+				println "Build wiperdog successfully!"
+			} else {
+				println "Build wiperdog failure!"
+				return
 			}
 
-			// get output when run command
-			output = runProcClosure(listCmd, dir, true)
-			def logFile = new File(pathToWiperdog + "target/WiperdogInstaller.log")
+			// set list test case will be run
+			def listCase = []
+			if (caseTest == "all" || caseTest == "All" || caseTest == "ALL") {
+				listCase = ["Case1", "Case2", "Case3"]
+			} else {
+				listCase.add(caseTest)
+			}
+
+			def logFile1 = new File(pathToWiperdog + "target/WiperdogInstaller.log")
+			def logFile2 = new File(pathToWiperdog + "target/WiperdogInstaller.log.1")
 			def logStr
 
-			// check log writed to file
-			if(logFile.exists()) {
-				// get string in log
-				logStr = logFile.getText()
-				// check result
-				checkResultData(caseTest, output, logStr)
-			} else {
-				println "Test failure!"			
+			// run test case
+			listCase.each{
+				caseTest = it
+				print "\n"
+				println "==========Test $caseTest=========="
+				// remove logfile
+				if(logFile1.exists()) {
+					logFile1.delete()
+				}
+				// remove logfile
+				if(logFile2.exists()) {
+					logFile2.delete()
+				}
+				listCmd = new LinkedList<String>();
+				listCmd.add("java")
+				listCmd.add("-jar")
+				listCmd.add(jarFile)
+				listCmd.add("-d")
+				listCmd.add(dirFolderTest + "/WiperdogTest")
+				listCmd.add("-j")
+				if (caseTest != "Case2") {//set jetty port is number
+					listCmd.add("123456")	
+				} else {//set jetty port is not number
+					listCmd.add("error")
+				}
+				if (caseTest != "Case3") {
+					listCmd.add("-m")
+					listCmd.add("10.0.0.2")
+					listCmd.add("-p")
+					listCmd.add("28017")
+					listCmd.add("-n")
+					listCmd.add("wiperdog")
+					listCmd.add("-u")
+					listCmd.add("sa")
+					listCmd.add("-pw")
+					listCmd.add("insight")
+					listCmd.add("-mp")
+					listCmd.add("test@gmail.com")
+					listCmd.add("-s")
+					listCmd.add("no")
+				}
+
+				// get output when run command
+				println "Extract wiperdog..."
+				output = runProcClosure(listCmd, new File(pathToWiperdog + "target"), true)
+				sleep(2000)
+
+				// check log writed to file
+				if(logFile1.exists()) {
+					// get string in log
+					logStr = logFile1.getText()
+					if(logFile2.exists()) {
+						logStr += logFile2.getText()								
+					}
+					// check result
+					println "Checking log..."
+					checkResultData(caseTest, logStr)
+					sleep(2000)
+				} else {
+					println "Test failure!"			
+				}
 			}
 		}catch(Exception ex){
 			ex.printStackTrace()
@@ -95,18 +126,24 @@ public class TestJobServlet{
 	 */
 	public static void checkResultData(String caseTest, String log) {
 		if (caseTest == "Case1") {// data when install wiperdog corresponding to dataset
+			println "**Input: Value of parameters without default (ex: jetty port is 123456)"
+			println "**Expected: in the log file will show value of parameters corresponding to set"			
 			if (log.contains("Server Port:123456")) {
 					println "Test $caseTest successfully!"		
 			} else {
 					println "Test $caseTest failure!"	
 			}
 		} else if (caseTest == "Case2") {// have error when install wiperdog
+			println "**Input: Value of jetty port to set is not number"
+			println "**Expected: in the log file will show the message about error"
 			if (log.contains("Jetty port must be number: -j")) {
 					println "Test $caseTest successfully!"		
 			} else {
 					println "Test $caseTest failure!"	
 			}
 		} else if (caseTest == "Case3") {// data when install wiperdog successfully is default
+			println "**Input: Value of parameters is default"
+			println "**Expected: in the log file will show value of parameters is default"
 			if (log.contains("Database address:127.0.0.1")) {
 					println "Test $caseTest successfully!"		
 			} else {
